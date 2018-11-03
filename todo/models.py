06329037328ef0c2996b232fdffa84de6d5d3feb
haeree <cocoa3217@gmail.com>
2018-11-04 03:07:37 +0900

    기한 지남 표시 수정과 model 안쓰는 컬럼 삭제

--- todo/models.py
+++ todo/models.py
@@ -5,24 +5,6 @@ from django.db import models
 
 class Todo (models.Model):
 
-    URGENT_IMPORTANT = 'UI'
-    NOT_URGENT_IMPORTANT = 'NI'
-    URGENT_NOT_IMPORTANT = 'UN'
-    NOT_URGENT_NOT_IMPORTANT = 'NN'
-
-    PRIORITY_CHOICES = (
-        (URGENT_IMPORTANT, 'urgent&important'),
-        (NOT_URGENT_IMPORTANT, 'not urgent&important'),
-        (URGENT_NOT_IMPORTANT, 'urgent&not important'),
-        (NOT_URGENT_NOT_IMPORTANT, 'not urgent&not important'),
-    )
-
-    priority = models.CharField(
-        max_length=2,
-        choices=PRIORITY_CHOICES,
-        default=NOT_URGENT_NOT_IMPORTANT,
-    )
-
     title = models.CharField(max_length=50)
     content = models.TextField()
     dueDate = models.DateField(
